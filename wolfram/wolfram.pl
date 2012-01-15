2f22904828e9417d11f2894d1bdf972d3d4532ef
Lefteris Zafiris <zaf.000@gmail.com>
2012-01-15 04:31:07 +0200

    Fixed keys() arguments for compatibility with older versions of perl.

--- wolfram/wolfram.pl
+++ wolfram/wolfram.pl
@@ -58,7 +58,7 @@ if ($answer->{success} eq 'false') {
 #print Dump($answer);
 my $results = 0;
 
-foreach (keys $answer->{pod}) {
+foreach (keys %{$answer->{pod}}) {
 	#print Dump($answer->{pod}->{$_});
 	if ($_ eq "subpod") {
 		print "$answer->{pod}->{$_}->{plaintext}\n";
@@ -76,7 +76,7 @@ foreach (keys $answer->{pod}) {
 	}
 }
 if (!$results) {
-	foreach (keys $answer->{pod}) {
+	foreach (keys ${$answer->{pod}}) {
 		eval{ print "$_: $answer->{pod}->{$_}->{subpod}->{plaintext}\n"; };
 		eval{ print "$_: $answer->{pod}->{$_}->{subpod}->[0]->{plaintext}\n"; };
 		$results++;
